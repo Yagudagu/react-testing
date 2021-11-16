d47ee011e38b435bdd6bdd68802e199a97131887
Jacob Martin <Jacobhmartian@gmail.com>
2021-11-16 16:45:58 -0700

    adding everything

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useMemo } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number);
+  }, [number]);
+  const themeStyles = useMemo(() => {
+    return {
+      backgroundColor: dark ? "black" : "white",
+      color: dark ? "white" : "black",
+    };
+  }, [dark]);
+
+  useEffect(() => {
+    console.log("Theme changed");
+  }, [themeStyles]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Change Theme
+      </button>
+      <div style={themeStyles}>{doubleNumber}</div>
+    </>
   );
+
+  function slowFunction(num) {
+    console.log("Calling Slow Function");
+    for (let i = 0; i <= 1000000000; i++) {}
+    return num * 2;
+  }
 }
+
+// From the Use Effect Lesson
+
+// function App() {
+//   // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+//   const [resourceType, setResourceType] = useState("posts");
+//   const [items, setItems] = useState([]);
+
+//   useEffect(() => {
+//     console.log("resource changed");
+
+//     return () => {
+//       console.log("return from resource change");
+//     };
+//   }, [resourceType]);
+
+//   // useEffect(() => {
+//   //   fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+//   //     .then((response) => response.json())
+//   //     .then((json) => setItems(json));
+//   // }, [resourceType]);
+
+//   // const handleResize = () => {
+//   //   setWindowWidth(window.innerWidth);
+
+//   //   return () => {
+//   //     window.removeEventListener("resize", handleResize);
+//   //   };
+//   // };
+
+//   // useEffect(() => {
+//   //   window.addEventListener("resize", handleResize);
+//   // }, []);
+
+//   return (
+//     // <div>{windowWidth}</div>
+
+//     <>
+//       <div>
+//         <button onClick={() => setResourceType("posts")}>Posts</button>
+//         <button onClick={() => setResourceType("users")}>Users</button>
+//         <button onClick={() => setResourceType("comments")}>Comments</button>
+//       </div>
+//       <h1>{resourceType}</h1>
+//       {items.map((item) => {
+//         return <pre>{JSON.stringify(item)}</pre>;
+//       })}
+//     </>
+//   );
+// }
 
 export default App;
---
+++ src/counter.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+const ErrorComponent = () => <div>{props.ignore}</div>;
+
+export default class Counter extends React.Component {
+  constructor(props) {
+    console.log("Constructor");
+    super(props);
+    this.state = {
+      counter: 0,
+      seed: 0,
+    };
+
+    this.increment = () => this.setState({ counter: this.state.counter + 1 });
+    this.decrement = () => this.setState({ counter: this.state.counter - 1 });
+  }
+
+  // This allows us to pass in props and add them to the state
+  static getDerivedStateFromProps(props, state) {
+    if (props.seed && state.seed !== props.seed) {
+      return {
+        seed: props.seed,
+        counter: props.seed,
+      };
+    }
+    return null;
+  }
+
+  // Gets called after the component mounts
+  componentDidMount() {
+    console.log("ComponentDidMount");
+    console.log("-----------------------------");
+  }
+
+  // Used to stop the component from updating
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      nextProps.ignoreProp &&
+      this.state.ignoreProp !== nextProps.ignoreProp
+    ) {
+      console.log("Should Component Update - DO NOT RENDER");
+      return false;
+    }
+    console.log("Should Component Update - RENDER");
+
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("GetSnapshotBeforeUpdate");
+    return null;
+  }
+
+  render() {
+    console.log("Render");
+
+    return (
+      <div>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+
+        <div className="counter">Counter: {this.state.counter}</div>
+        <ErrorComponent />
+      </div>
+    );
+  }
+
+  // This is called when the component is updated
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("Component Did Update");
+    console.log("-----------------------------");
+  }
+
+  // Called after the component unmounts
+  componentWillUnmount() {
+    console.log("ComponentWillUnmount");
+    console.log("-----------------------------");
+  }
+
+  componentDidCatch(error, info) {
+    console.log("ComponentDidCatch");
+    console.log("-----------------------------");
+  }
+}
